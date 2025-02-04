6d8a3d3c3e9ada313a3d612a11662cbb892f2c7b
Ulash Yshyk <uyshyk@myseneca.ca>
2025-02-04 10:27:01 -0500

    AS2 P1S3

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const path = require('path');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Ulash Yshyk - 153349220');
+  res.send(`
+    <h1>Ulash Yshyk - 153349220</h1>
+    <p><a href="/about">Go to About Page</a></p>
+  `);
 });
-
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
