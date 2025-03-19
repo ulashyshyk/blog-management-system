eadef41ca1e354ad56507edb5572b4cab412a150
Ulash Yshyk <uyshyk@myseneca.ca>
2025-03-19 10:32:40 -0400

    AS4 Category Name

--- content-service.js
+++ content-service.js
@@ -66,7 +66,7 @@ module.exports.addArticle = (articleData) => {
 
 module.exports.getArticlesByCategory = (category) => {
     return new Promise((resolve, reject) => {
-    const filteredArticles= articles.filter(article=> article.category == category);
+    const filteredArticles = articles.filter(article => article.category == category);
     if (filteredArticles.length > 0) resolve(filteredArticles);
     else reject("no results returned");
     });
@@ -83,7 +83,7 @@ module.exports.getArticlesByMinDate = (minDateStr) => {
 
 module.exports.getArticleById = (id) => {
     return new Promise((resolve, reject) => {
-    const foundArticle = articles.find(article=> article.id == id);
+    const foundArticle = articles.find(article => article.id == id);
     if (foundArticle) resolve(foundArticle);
     else reject("no result returned");
     });
--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.get('/categories',(req,res) => {
       return getCategories()
     })
     .then((categories) =>{
-      res.render('categories.ejs',{ categories: categories})
-    }).catch((err) => {
+      res.render('categories.ejs',{categories : categories})
+     }).catch((err) => {
       console.log(err)
       res.status(500).json({message : err})
     })
@@ -83,61 +83,61 @@ app.get('/articles/add', (req, res) => {
   res.render('addArticle.ejs')
   })
 
-  app.post('/articles/add', upload.single("featureImage"), (req, res) => {
-    if (req.file) {  //checks if  file exists
-      let streamUpload = (req) => {
-       return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream(   //upload stream to Cloudinary
-          (error, result) => {
-            if (result) resolve(result);
-            else reject(error);
-            }
-          );
-            streamifier.createReadStream(req.file.buffer).pipe(stream);  //converts uploaded file into readable stream and pipes it into Cloudinary upload stream
-          });
-      };
-      async function upload(req) {     
-        let result = await streamUpload(req);  //waits for upload to finish then returns result 
-        return result;
-      }
-
-      upload(req).then((uploaded) => {
-        processArticle(uploaded.url);     //uploads the image to cloudinary if successful
-      }).catch(err => res.status(500).json({ message: "Image upload failed", error: err }));
-      } else {
-      processArticle("");   //if no file was uploaded,file is set to an empty string
-      }
-      function processArticle(imageUrl) {    
-        req.body.featureImage = imageUrl;  //attachs image url to the article data
-        // Add article to content-service
-      contentService.addArticle((req.body)
-      .then(() => res.redirect('/articles'))  //saves the article,if successful redirects the user ro /articles
-      .catch(err => res.status(500).json({ message: "Article creation failed", error: err })));
-      }
-     });
-
-     app.get("/articles/:id",(req,res) => {
-      initialize()
-        .then(() => {
-          return getPublishedArticles()
-        })
-        .then((articles) =>{
-          const id = parseInt(req.params.id)
-    
-          let filteredArticles = articles
-    
-          filteredArticles = filteredArticles.filter(article => article.id === id)
-    
-          if(filteredArticles.length > 0){
-              res.json(filteredArticles)
-          }else{
-            res.status(404).json({message:"Article not found"})
+app.post('/articles/add', upload.single("featureImage"), (req, res) => {
+  if (req.file) {  //checks if  file exists
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream(   //upload stream to Cloudinary
+        (error, result) => {
+          if (result) resolve(result);
+          else reject(error);
           }
-        }).catch((err) => {
-          console.log(err)
-          res.status(500).json({message : err})
-        })
+        );
+          streamifier.createReadStream(req.file.buffer).pipe(stream);  //converts uploaded file into readable stream and pipes it into Cloudinary upload stream
+        });
+    };
+    async function upload(req) {     
+      let result = await streamUpload(req);  //waits for upload to finish then returns result 
+      return result;
+    }
+
+    upload(req).then((uploaded) => {
+      processArticle(uploaded.url);     //uploads the image to cloudinary if successful
+    }).catch(err => res.status(500).json({ message: "Image upload failed", error: err }));
+    } else {
+    processArticle("");   //if no file was uploaded,file is set to an empty string
+    }
+    function processArticle(imageUrl) {    
+      req.body.featureImage = imageUrl;  //attachs image url to the article data
+      // Add article to content-service
+    contentService.addArticle((req.body)
+    .then(() => res.redirect('/articles'))  //saves the article,if successful redirects the user ro /articles
+    .catch(err => res.status(500).json({ message: "Article creation failed", error: err })));
+    }
+});
+
+app.get("/articles/:id",(req,res) => {
+  initialize()
+    .then(() => {
+      return getPublishedArticles()
     })
+    .then((articles) =>{
+      const id = parseInt(req.params.id)
+
+      let filteredArticles = articles
+
+      filteredArticles = filteredArticles.filter(article => article.id === id)
+
+      if(filteredArticles.length > 0){
+          res.json(filteredArticles)
+      }else{
+        res.status(404).json({message:"Article not found"})
+      }
+    }).catch((err) => {
+      console.log(err)
+      res.status(500).json({message : err})
+    })
+})
 app.listen(port, () => {
   console.log(`Express http server listening on port ${port}`);
   console.log(`http://localhost:${port}`)
